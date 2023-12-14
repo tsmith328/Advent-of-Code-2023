65a607bf6460b92df31726c2ed3350b549e2457b
Tyler Smith <t.m.s015@gmail.com>
2023-12-13 19:36:13 -0500

    Complete Day 12

--- Day 12/day12_program.py
+++ Day 12/day12_program.py
@@ -1,8 +1,8 @@
 import os
 from itertools import combinations
+from functools import cache
 
 test_file = "E:/Documents/Advent of Code/Advent-of-Code-2023/Day 12/day12_input_test.txt"
-test_file = "C:/Users/tms/Downloads/AOC/Advent-of-Code-2023/Day 12/day12_input_test.txt"
 
 input_file = "E:/Documents/Advent of Code/Advent-of-Code-2023/Day 12/day12_input.txt"
 
@@ -49,38 +49,46 @@ def get_combos(input_data):
     
     return total_combos
 
+@cache
 def get_combos_rec(springs: str, groups: list[int]) -> int:
+    #print(f"get_combos_rec({springs},{groups})")
     # Start with base cases
     if len(springs) == 0:
         if len(groups) == 0:
             # Nothing left, we can consider it a success
+            #print("Return 1")
             return 1
         else:
             # No more springs left, but we have groups of broken springs left
+            #print("Return 0")
             return 0
     
     if len(groups) == 0:
         # No broken springs should be left
         if "#" in springs:
             # Broken springs are left
+            #print("Return 0.")
             return 0
         else:
             # None left, yay.
+            #print("Return 1")
             return 1
     
     # we have some springs and some groups left.
     next_spring = springs[0]
-    next_group = groups[0]
     if next_spring == '.':
         # If first char is a dot, handle it
+        #print("Run dot")
         return get_combos_dot(springs, groups)
     
     if next_spring == '#':
         # If a hash/pound, handle it
+        #print("Run Pound")
         return get_combos_pound(springs, groups)
     
     # Next spring must be ?
-    return get_combos_dot(springs, groups) + get_combos_pound(springs.replace('?', '#', 1), groups)
+    #print("Run Both")
+    return get_combos_dot(springs, groups) + get_combos_pound(springs, groups)
     
 def get_combos_pound(springs: str, groups: list[int]) -> int:
     # This is called when the next character is a #, which means the first N chars need to be # or ?
@@ -89,16 +97,19 @@ def get_combos_pound(springs: str, groups: list[int]) -> int:
     if n > len(springs):
         return 0
     next_n = springs[:n] # chars that need to be broken for this group to be ok
-    if '.' in next_n:
+    next_n = next_n.replace('?', '#')
+    if next_n != "#" * n:
         # If a . is here, we don't have enough room to fit all the broken springs
         return 0
     if n == len(springs):
         # length of springs is the same as the group that's left and we have no .'s.
-        return 1
+        if len(groups) == 1:
+            return 1
+        return 0
     next_char = springs[n] # Next character after this group of broken springs. Should be unbroken.
     if next_char in '?.':
         # It can be unbroken. Check the rest of the string.
-        return get_combos_rec(springs[n:], groups[1:])
+        return get_combos_rec(springs[n+1:], groups[1:])
     
     # Next char is broken, this group is too big
     return 0        
@@ -126,7 +137,7 @@ def find_combos_recursively(input_data: list[str]) -> int:
         springs = line.split()[0]
         groups = tuple([int(x) for x in line.split()[1].split(',')])
         combos = get_combos_rec(springs, groups)
-        print(f"{line} -> {combos}.")
+        #print(f"{line} -> {combos}.")
         total_combos += combos
     
     return total_combos
@@ -139,7 +150,7 @@ def run_case(file_name: str) -> str:
 
     input_data = unfolded(input_data)
     total_combos_2 = 0
-    total_combos_2 = get_combos(input_data) 
+    total_combos_2 = find_combos_recursively(input_data) 
 
     return f"The total number of valid combinations is: {total_combos_1}." \
           + f"{os.linesep}\tThe total number of valid unfolded combinations is: {total_combos_2}."
