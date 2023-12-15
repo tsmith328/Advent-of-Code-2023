063b63050bf34431952a92a93ec38a21a055cc0c
Tyler Smith <tms@phxholdings.com>
2023-12-15 10:24:47 -0500

    Complete Day 15

--- Day 15/day15_program.py
+++ Day 15/day15_program.py
@@ -26,10 +26,52 @@ def hash(data: str) -> int:
 def hash_steps(steps: list[str]) -> int:
     return sum(map(hash, steps))
 
+def find_index(list: list[tuple[str, int]], label: str) -> int:
+    for i, item in enumerate(list):
+        if item[0] == label:
+            return i
+    return -1
+
+def build_lens_boxes(steps: list[str]) -> dict[int, list[tuple[str, int]]]:
+    boxes = dict([(i, []) for i in range(256)])
+    for step in steps:
+        if '=' in step:
+            # Replace or add
+            label, val = step.split('=')
+            hash_val = hash(label)
+            box = boxes[hash_val]
+            val = int(val)
+            idx = find_index(box, label)
+            if idx > -1:
+                box[idx] = (label, val)
+            else:
+                box.append((label, val))
+        else:
+            # Remove
+            label = step.replace('-', '')
+            hash_val = hash(label)
+            box = boxes[hash_val]
+            idx = find_index(box, label)
+            if idx > -1:
+                box.pop(idx)
+
+    return boxes
+
+def calc_focal_power(boxes: dict[int, list[tuple[str,int]]]) -> int:
+    power = 0
+    for box_num, lenses in boxes.items():
+        for i, lens in enumerate(lenses):
+            power += ((box_num + 1) * (i + 1) * lens[1])
+    
+    return power
+
+
 def run_case(file_name: str) -> str:
     input_data = read_file(file_name)
 
-    hash_val = hash_steps(input_data.split(','))
+    steps = input_data.split(',')
+
+    hash_val = hash_steps(steps)
 
     # Part 2:
     # 1. Create a hashmap/dictionary using hash function as key
@@ -37,8 +79,12 @@ def run_case(file_name: str) -> str:
     # 3. Look up which list to use. Then follow directions from step
     # 4. Traverse dict.items() to calculate focusing power.
 
+    boxes = build_lens_boxes(steps)
+
+    focal_power = calc_focal_power(boxes)
+
     return f"The total hash value is: {hash_val}." \
-          + f"{os.linesep}\tThe total weight after the spin cycle is: {''}."
+          + f"{os.linesep}\tThe total focal power is: {focal_power}."
 
 def main() -> None:
     # Run test case
